019a37038ef4c2955474b6b54b6632db9958ebe9
kotfranek <p.podwapinski@gmail.com>
2016-04-16 13:18:19 +0200

    Shorten the UDP commands

--- include/trace/backend/udp/UdpBackEndControl.h
+++ include/trace/backend/udp/UdpBackEndControl.h
@@ -29,28 +29,28 @@ namespace trace
             const int64_t UDP_SERVER_HEARTBEAT_TIMEOUT = 1000U * 1000U; 
                                 
             /* UDP Client identification */
-            const char* UDP_CLIENT_HANDSHAKE = "TRACELOG_UDP_CLIENT_HS";  
+            const char* UDP_CLIENT_HANDSHAKE = "TL_UDP_CL_HS";  
 
             /* UDP Client Disconnect */
-            const char* UDP_CLIENT_CLOSE = "TRACELOG_UDP_CLIENT_CLOSE";
+            const char* UDP_CLIENT_CLOSE = "TL_UDP_CL_CLOSE";
 
             /* UDP Client Disconnect */
-            const char* UDP_CLIENT_ID = "TRACELOG_UDP_CLIENT_ID";      
+            const char* UDP_CLIENT_ID = "TL_UDP_CL_ID";      
             
             /* UDP Client Present (CURRENTLY NOT USED) */
-            const char* UDP_CLIENT_PRESENT = "TRACELOG-CLIENT-PRESENT";
+            const char* UDP_CLIENT_PRESENT = "TL_UDP_CL_PR";
             
             /* UDP Server Log Entry prefix */
-            const char* UDP_SERVER_LOG_ENTRY = "TL-LE";
+            const char* UDP_SERVER_LOG_ENTRY = "TL_LE";
 
             /* UDP Server identification */
-            const char* UDP_SERVER_HANDSHAKE = "TRACELOG_UDP_SRV_HS";    
+            const char* UDP_SERVER_HANDSHAKE = "TL_UDP_SRV_HS";    
 
             /* UDP Server Heartbeat signal */
-            const char* UDP_SERVER_HEARTBEAT = "TRACELOG_UDP_SRV_HB";
+            const char* UDP_SERVER_HEARTBEAT = "TL_UDP_SRV_HB";
 
             /* UDP Client Disconnect */
-            const char* UDP_SERVER_CLOSE = "TRACELOG_UDP_SRV_CLOSE";                
+            const char* UDP_SERVER_CLOSE = "TL_UDP_SRV_CLOSE";                
         }
     }
 };
